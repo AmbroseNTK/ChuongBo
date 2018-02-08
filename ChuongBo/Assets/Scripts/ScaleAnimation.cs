5770117fd4319e137350c294f97cf00b0e5fbb0f
Ambrose Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2018-02-08 14:37:40 +0700

    Add Button UI
    Buttons and Animations

---
+++ ChuongBo/Assets/Scripts/ScaleAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAnimation : MonoBehaviour {
+
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+    public void Open()
+    {
+        animator.SetBool("isOpen", true);
+    }
+    public void Close()
+    {
+        animator.SetBool("isOpen", false);
+    }
+    public void Toggle()
+    {
+        animator.SetBool("isOpen", !animator.GetBool("isOpen"));
+    }
+}
